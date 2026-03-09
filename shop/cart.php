5810f3b49a05a4569cbb1f84859bf7f79c3a552d
Chinnapat <66010914104@msu.ac.th>
2026-03-09 20:06:10 +0700

    5611

--- shop/cart.php
+++ shop/cart.php
@@ -251,7 +251,6 @@ $netTotal = $totalPrice + $shippingFee; // ยอดสุทธิ
                 </a>
                 <a href="cart.php" class="hover:text-primary transition relative flex items-center">
                     <span class="material-icons-round text-2xl text-primary">shopping_bag</span>
-                    <span class="absolute -top-1.5 -right-2 bg-primary text-white text-[10px] font-bold rounded-full h-[18px] w-[18px] flex items-center justify-center border-2 border-white dark:border-gray-800">
                     </span>
                 </a>
                 
